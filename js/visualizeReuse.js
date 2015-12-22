8ec6bdbc375980928a6efe062acc82a8da4fd52d
duhaime <douglas.duhaime@gmail.com>
2015-12-21 20:30:35 -0500

    introduced key function and update model

--- js/visualizeReuse.js
+++ js/visualizeReuse.js
@@ -8,15 +8,15 @@ var makePlotCall = function(sourceId){
   var jsonDir = "json/alignments/"; 
   var jsonFile = sourceId + "_alignments.json";
   var jsonPath = jsonDir + jsonFile
-  $.getJSON( jsonPath, function( data ) {
-    var sliced_data = data.slice();
+  $.getJSON( jsonPath, function( jsonResponse ) {
+    var sliced_data = jsonResponse.slice();
     makeScatterPlot( sliced_data );
   });
 };  
 
 // populate dropdown with json options
-$.getJSON( "json/dropdown.json", function( data ) {
-  $.each(data, function (key, value) {
+$.getJSON( "json/dropdown.json", function( jsonResponse ) {
+  $.each(jsonResponse, function (key, value) {
     $("#textSelector").append($('<option></option>').val(value.id).html(value.name));
   });
   // initialize scatterplot
@@ -25,8 +25,6 @@ $.getJSON( "json/dropdown.json", function( data ) {
 
 // event handler for change of dropdown
 $('#textSelector').change(function () {
-  // remove current plot svg
-  d3.select("#scatterPlot").select("svg").remove();
   makePlotCall( $(this).val() );
 });
 
@@ -56,12 +54,12 @@ var updateText = function(d) {
   d3.select("#titleRight").html( d.similarTitle);
 
   // retrieve source segment
-  $.getJSON( "json/segments/segments_" + d.sourceId + ".json", function( data ) {
-    d3.select("#textLeft").html( data[d.sourceSegment] );
+  $.getJSON( "json/segments/segments_" + d.sourceId + ".json", function( jsonResponse ) {
+    d3.select("#textLeft").html( jsonResponse[d.sourceSegment] );
   });
   // retrieve similar segment
-  $.getJSON( "json/segments/segments_" + d.similarId + ".json", function( data ) {
-    d3.select("#textRight").html( data[d.similarSegment] );
+  $.getJSON( "json/segments/segments_" + d.similarId + ".json", function( jsonResponse ) {
+    d3.select("#textRight").html( jsonResponse[d.similarSegment] );
   });
 };
 
@@ -78,114 +76,127 @@ var resetText = function() {
 var similarityFn = function(d) { return d.similarity }
 var segmentFn = function(d) { return d.sourceSegment }
 
+// width and height
+var margin = {top: 0, right: 400, left: 50, bottom: 30}   
+var w = 750 - margin.left - margin.right;
+var h = 230 - margin.top - margin.bottom;
+
+var x = d3.scale.linear()
+  .range([15, w-15]);
+
+var y = d3.scale.linear()
+  .range([h-15, 15]);
+
+var svg = d3.select("#scatterPlot").append("svg:svg")
+  .attr("width", w + margin.left + margin.right)
+  .attr("height", h + margin.top + margin.bottom);
+
+// select a subregion of the svg to create a dropbox
+var graphBox = svg.append("rect")
+   .attr("id", "graphBox")
+   .attr("x", margin.left)
+   .attr("y", margin.top)
+   .attr("height", h)
+   .attr("width", w)
+   .attr("stroke", "#c4c4c4")
+   .attr("stroke-width", 1)
+   .attr("fill", "#ffffff");
+
+// draw x-axis
+var xAxis = d3.svg.axis()
+  .scale(x)
+  // limit x-axis to integers only
+  .tickFormat(function(e){
+      if(Math.floor(e) != e)
+      { return;}
+      return e;
+  });
+
+// append x-axis to svg
+var xAxisGroup = svg.append("g")
+  .attr("class","x axis")
+  .attr("transform", "translate(" + margin.left + "," + h + ")");
+
+// draw y axis
+var yAxis = d3.svg.axis()
+  .scale(y)
+  .orient("left")
+         
+// append y-axis to svg
+var yAxisGroup = svg.append("g")
+  .attr("class", "y axis")
+  .attr("transform", "translate(" + margin.left + ", 0)")
+
+// use d.similarId+d.similarity as key function
+var dataKey = function(d) {
+  return d.similarId + d.similarity;
+};
+
 // main plotting function
 var makeScatterPlot = function(data) {
-
-  // reset text in the textBox
-  resetText();
 
   // specify color scheme
   var colors = d3.scale.category20();
 
-  // width and height
-  var margin = {top: 0, right: 400, left: 50, bottom: 30}   
-  var w = 750 - margin.left - margin.right;
-  var h = 230 - margin.top - margin.bottom;
+  // reset text in the textBox
+  resetText();
 
-  var x = d3.scale.linear()
-    .range([15, w-15])
-    .domain(d3.extent(data, segmentFn))
+  // set x and y domains
+  x.domain(d3.extent(data, segmentFn))
+  y.domain(d3.extent(data, similarityFn))
 
-  var y = d3.scale.linear()
-    .range([h-15, 15])
-    .domain(d3.extent(data, similarityFn))
+  // update x and y axes
+  xAxisGroup.call(xAxis); 
+  yAxisGroup.call(yAxis);  
 
-  var svg = d3.select("#scatterPlot").append("svg:svg")
-    .attr("width", w + margin.left + margin.right)
-    .attr("height", h + margin.top + margin.bottom);
+  // specify data with key function
+  var circles = svg.selectAll("circle").data(data, dataKey);
+  circles.transition(500)
+    .attr("cx", function(d) { return x(segmentFn(d)) + margin.left })
+    .attr("cy", function(d) { return x(similarityFn(d)) + margin.top })
+    .attr("stroke", function(d) {return colors(d.similarId)})
+    .attr("class", "scatterPoint")
 
-  // select a subregion of the svg to create a dropbox
-  var graphBox = svg.append("rect")
-     .attr("class", "graphBox")
-     .attr("x", margin.left)
-     .attr("y", margin.top)
-     .attr("height", h)
-     .attr("width", w)
-     .attr("stroke", "#c4c4c4")
-     .attr("stroke-width", 1)
-     .attr("fill", "#ffffff")
-
-  // append circles for each observation
-  svg.selectAll("circle").data(data).enter()
+  circles.enter()
     .append("svg:circle")
     .attr("r", 4)
+    .attr("similarity", function(d) { return d.similarity })
     .attr("cx", function(d) { return x(segmentFn(d)) + margin.left })
-    .attr("cy", function(d) { return y(similarityFn(d)) + margin.top })
+    .attr("cy", function(d) { return y(similarityFn(d)) })
     .attr("style", "cursor: pointer;")
     .attr("stroke", function(d) {return colors(d.similarId)})
     .on("click", function(d) {
       updateText(d)
     });
  
-  // draw x-axis
-  var xAxis = d3.svg.axis()
-    .scale(x)
-    // limit x-axis to integers only
-    .tickFormat(function(e){
-        if(Math.floor(e) != e)
-        { return;}
-        return e;
-    });
-
-  // append x-axis to svg
-  var xAxisGroup = svg.append("g")
-    .attr("class","x axis")
-    .attr("transform", "translate(" + margin.left + "," + h + ")")
-    .call(xAxis);
-  
-  // draw y axis
-  var yAxis = d3.svg.axis()
-    .scale(y)
-    .orient("left")
-         
-  // append y-axis to svg
-  var yAxisGroup = svg.append("g")
-    .attr("class", "y axis")
-    .attr("transform", "translate(" + margin.left + ", 0)")
-    .call(yAxis);  
-
-  // build legend 
-  var legendDiv = svg.append("g")
-    .attr("class", "legend")
-    .attr("x", w-margin.left)
-    .attr("y", h)
-    .attr("height", h)
-    .attr("width", margin.right);
+  circles.exit().remove();
 
   // retrieve one observation of each similarId
   var uniqueIds = uniquify(data);
 
-  var legend = legendDiv.selectAll('.legend')                     
-      .data(uniqueIds)                                   
-      .enter()                                                
-      .append('g')                                            
-      .attr('class', 'legend')                                
-      .each(function(d, i) {
-        var g = d3.select(this);
-        g.append("svg:circle")
-          .attr("cx", w + margin.left + 24)
-          .attr("cy", 20*i+15)
-          .attr("r", 4)
-          .style("stroke", function(d){return colors(d.similarId)});
+  var legends = svg.selectAll(".legend").data(uniqueIds, dataKey); 
+  legends.transition(500)
+    .attr("stroke", function(d) { return colors(d.similarId) })
+    .text(function(d){return d.similarTitle});
+
+  legends.enter()                                           
+    .append('g')                                           
+    .attr('class', 'legend')                                
+    .each(function(d, i) {
+      var g = d3.select(this);
+      g.append("svg:circle")
+        .attr("cx", w + margin.left + 24)
+        .attr("cy", 20*i+15)
+        .attr("r", 4)
+        .style("stroke", function(d){return colors(d.similarId)});
         
-        g.append("text")
-          .attr("x", w + margin.left + 32)
-          .attr("y", 20*i+20)
-          .attr("height",20)
-          .attr("width",60)
-          .style("fill", "#000000")
-          .text(function(d){return d.similarTitle});      
-     });
+      g.append("text")
+        .attr("x", w + margin.left + 32)
+        .attr("y", 20*i+20)
+        .attr("height",20)
+        .attr("width",60)
+        .style("fill", "#000000")
+        .text(function(d){return d.similarTitle});      
+   });
+  legends.exit().remove();
 };
-
-
